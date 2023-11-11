6d617583c45815d7bf98a480ff41fd3eb05c7e24
bikiris <a664347635@gmail.com>
2023-11-10 19:53:33 -0500

    Add button that prompts grid size

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ for(let i = 0; i < 16; i++){
 
 const box = document.querySelectorAll(".box");
 box.forEach((element) => {
-    element.addEventListener('mouseover', () => {
+    element.addEventListener('mouseover', ()=> {
         element.style.backgroundColor = "red";
     })
-    element.addEventListener('mouseout', () => {
+    element.addEventListener('mouseout', ()=> {
         element.style.backgroundColor = "white";
     })
 })
+
+const btn = document.querySelector(".btn");
+let gridSize = 0;
+btn.addEventListener('click', ()=> {
+    gridSize = prompt("Enter a number less than 100." , 0);
+});
