4839b96ca3ab26d4f3b8dc16e4b2017d23bc8279
bikiris <a664347635@gmail.com>
2023-11-10 21:21:02 -0500

    Resize grids and add hover effect

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-function createGrid(){
-    const container = document.querySelector(".content");
-    const box = document.createElement("div");
-    box.className = "box";
-    container.appendChild(box);
+function createGrid(gridSize){
+    const container = document.querySelector('.content');
+    container.textContent = "";
+    
+    for(let i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.className = "row";
+        for(let j = 0; j < gridSize; j++){
+            const box = document.createElement('div');
+            box.className = "box";
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
 }
 
-for(let i = 0; i < 16; i++){
-    createGrid();
-}
 
-const box = document.querySelectorAll(".box");
-box.forEach((element) => {
-    element.addEventListener('mouseover', ()=> {
-        element.style.backgroundColor = "red";
-    })
-    element.addEventListener('mouseout', ()=> {
-        element.style.backgroundColor = "white";
-    })
-})
+
+
 
 const btn = document.querySelector(".btn");
 let gridSize = 0;
 btn.addEventListener('click', ()=> {
-    gridSize = prompt("Enter a number less than 100." , 0);
+    gridSize = prompt("Enter your grid size." , 0);
+    if(gridSize > 100){
+        alert("Grid size is too large");
+        return;
+    }
+    createGrid(gridSize);
+    addHover();
+    
 });
+
+function addHover(){
+    const box = document.querySelectorAll(".box");
+    box.forEach((element) => {
+        element.addEventListener('mouseover', changeColor);
+    });
+}
+
+function changeColor(event){
+    event.target.style.backgroundColor = "red";
+}
+
